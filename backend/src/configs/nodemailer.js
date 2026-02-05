3f4d56f65cc462027604d3f1d5dac13f2ad6ad46
falguni-mondal <career.falguni@gmail.com>
2026-02-05 16:10:41 +0530

    Trying to correct the nodemailer for mails

--- backend/src/configs/nodemailer.js
+++ backend/src/configs/nodemailer.js
@@ -3,15 +3,14 @@ import nodemailer from "nodemailer";
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // MUST be false for 587
+      service: "gmail",
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
       },
-      connectionTimeout: 10_000,
-      greetingTimeout: 10_000,
     });
 
     await transporter.sendMail({
@@ -23,7 +22,7 @@ export const sendEmail = async ({ to, subject, html }) => {
 
     return true;
   } catch (err) {
-    console.error("Email error:", err.message);
+    console.error("Email error:", err);
     return false;
   }
 };
